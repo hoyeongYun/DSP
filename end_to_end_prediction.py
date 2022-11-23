026945c953a6554af15af0a8cf399c482114c8b5
hoyeoungYun <yhy17520@hanyang.ac.kr>
2022-11-23 07:42:27 +0000

    full train 600 ver

--- end_to_end_prediction.py
+++ end_to_end_prediction.py
@@ -22,7 +22,7 @@ device = torch.device('cuda')
 split_strategy = 2
 batch_size = 4
 lr = 1e-3
-epochs = 400
+epochs = 600
 in_features = 8*18
 out_features = 18
 inter_dim = 1024
@@ -82,7 +82,7 @@ with torch.no_grad():
 
 result = result.reshape(642, -1)
 result_df = pd.DataFrame(result)
-result_df.to_csv('/workspace/DSP/result/ete/full_train_1e3_400_ver.csv', index=None)
+result_df.to_csv('/workspace/DSP/result/ete/full_train_1e3_600_ver.csv', index=None)
 
 
 
